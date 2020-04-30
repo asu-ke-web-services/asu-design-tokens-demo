54a73c72a1286ccce6296f52b06807b8ec8e8269
Nathan Rollins <nathan.rollins@asu.edu>
2020-04-29 21:28:05 -0700

    Add additional example transforms and build targets

--- config.js
+++ config.js
@@ -1,5 +1,23 @@
 const StyleDictionary = require('style-dictionary');
 
+const buildPath = 'build/';
+
+// You can still add custom transforms and formats like you
+// normally would and reference them in the config below.
+StyleDictionary.registerTransform({
+  name: 'myRegisteredTransform',
+  type: 'value',
+  matcher: (prop) => prop.attributes.category === 'size',
+  transformer: (prop) => `${parseInt(prop.value) * 16}px`
+});
+
+StyleDictionary.registerFormat({
+  name: 'myRegisteredFormat',
+  formatter: (dictionary) => {
+    return dictionary.allProperties.map((prop) => prop.value).join('\n');
+  }
+})
+
 const propertiesToCTI = {
   'width': {category: 'size', type: 'dimension'},
   'min-width': {category: 'size', type: 'dimension'},
@@ -36,6 +54,8 @@ const CTITransform = {
   }
 }
 
+// You can export a plain JS object and point the Style Dictionary CLI to it,
+// similar to webpack.
 module.exports = {
   source: ['properties/**/*.json', 'components/**/*.json'],
 
@@ -43,19 +63,60 @@ module.exports = {
   // directly in our configuration. Using .registerTransform() with the same
   // transform name, 'attribute/cti', would work as well.
   transform: {
-    // Override the attribute/cti transform
-    'attribute/cti': CTITransform
+    // Override the attribute/cti transform with our component-transform
+    'attribute/cti': CTITransform,
+
+    // Now we can use our custom transform 'myTransform'
+    myTransform: {
+      type: 'name',
+      transformer: (prop) => prop.path.join('_').toUpperCase()
+    }
+  },
+  // Same with formats, you can now write them directly to this config
+  // object. The name of the format is the key.
+  format: {
+    myFormat: (dictionary, platform) => {
+      return dictionary.allProperties.map(prop => `${prop.name}: ${prop.value}`).join('\n');
+    }
   },
 
   platforms: {
+    custom: {
+      // Using the custom transforms we defined above
+      transforms: ['attribute/cti', 'myTransform', 'myRegisteredTransform', 'color/hex'],
+      buildPath: buildPath,
+      files: [{
+        destination: 'variables.yml',
+        // Using the custom format defined above
+        format: 'myFormat'
+      }]
+    },
+    css: {
+      transformGroup: 'css',
+      buildPath: buildPath,
+      files: [{
+        destination: 'variables.css',
+        format: 'css/variables'
+      }]
+    },
     scss: {
       // We can still use this transformGroup because we are overriding
       // the underlying transform
       transformGroup: "scss",
-      buildPath: 'build/scss/',
+      buildPath: buildPath,
       files: [{
         destination: 'variables.scss',
         format: 'scss/variables'
+      }]
+    },
+
+    // You can still use built-in transformGroups and formats like before
+    json: {
+      transformGroup: 'js',
+      buildPath: buildPath,
+      files: [{
+        destination: 'properties.json',
+        format: 'json'
       }]
     }
   }
